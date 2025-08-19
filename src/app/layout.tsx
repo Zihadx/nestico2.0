4a883894e90776c35d873feaefa17fb80f47f900
zihadx <blankzihad@gmail.com>
2025-08-19 17:28:49 +0600

    clean some unusable code from navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/Shared/Footer/Footer";
 import ParallaxWrapper from "@/utils/ParallaxWrapper";
-import Navbar1 from "@/components/Shared/Navbar/Navbar1";
+import Navbar from "@/components/Shared/Navbar/Navbar";
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -18,7 +18,8 @@ const robotoMono = Roboto_Mono({
 
 export const metadata = {
   title: "Nestico - Your Trusted Home Service Partner",
-  description: "Upgrade your space with Nestico — Bangladesh’s trusted platform for walk-in showers, cleaning, plumbing, kitchen remodeling, and more. Stress-free, stylish, and always on time.",
+  description:
+    "Upgrade your space with Nestico — Bangladesh’s trusted platform for walk-in showers, cleaning, plumbing, kitchen remodeling, and more. Stress-free, stylish, and always on time.",
   icons: {
     icon: "/images/logo/nesticoFav.png",
   },
@@ -34,9 +35,9 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${robotoMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <Navbar1 />
-        
-         <ParallaxWrapper>
+        <Navbar />
+
+        <ParallaxWrapper>
           <main className="flex-grow">{children}</main>
         </ParallaxWrapper>
 
